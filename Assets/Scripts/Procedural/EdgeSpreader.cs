2a44f06cc8323201038494de7af31221c89e9aeb
Rick Dörr <Rick.nl@hotmail.com>
2026-01-03 17:01:47 +0100

    Wingspread setup

--- Assets/Scripts/Procedural/EdgeSpreader.cs
+++ Assets/Scripts/Procedural/EdgeSpreader.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using System.Collections.Generic;
 
 namespace LandOfTheConsumers.Procedural
 {
@@ -8,5 +9,36 @@ namespace LandOfTheConsumers.Procedural
     public class EdgeSpreader : MonoBehaviour
     {
         [HideInInspector] public EdgeData edgeData;
+        private List<Vector2Int> edgePoints;
+        private Vector2 perpendicular;
+
+        public void SetupEdgeSpread()
+        {
+            // Get edgeData from the EdgePairGenerator on the same GameObject
+            EdgePairGenerator edgePairGenerator = GetComponent<EdgePairGenerator>();
+            if (edgePairGenerator == null)
+            {
+                Debug.LogError("[EdgeSpreader] EdgePairGenerator component not found on this GameObject!");
+                return;
+            }
+
+            edgeData = edgePairGenerator.edgeData;
+
+            // Get the perpendicular vector from edge data
+            perpendicular = edgeData.perpendicularDirection;
+
+            // Copy all edge points
+            edgePoints = new List<Vector2Int>(edgeData.edgePixels);
+
+            SpreadWings();
+        }
+
+        private void SpreadWings()
+        {
+            for (int i = 0; i < edgePoints.Count; i++)
+            {
+
+            }
+        }
     }
 }
